55fb56189e87eea3199a1d86fa6f36c40d0f24f2
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-05-27 17:52:47 -0300

    Small changes to main functionality

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
-function add(num1,num2) {
-    return num1 + num2
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function subtract(num1,num2) {
-    return num1 - num2
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function multiply(num1,num2) {
-    return num1 * num2
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-function divide(num1,num2) {
-    return num1 / num2
+function divide(num1, num2) {
+  return num1 / num2;
 }
 
-function operate(num1,num2,operator) {
-    if (operator == "+") {
-        return add(num1,num2)
-    }
-    else if (operator == "-") {
-        return subtract(num1,num2)
-    }
-    else if (operator == "*") {
-        return multiply(num1,num2)
-    }
-    else if (operator == "/") {
-        return divide (num1,num2)
-    }
-    else {
-        return "Valid operator please"
-    }
+function operate(num1, num2, operator) {
+  if (operator == "+") {
+    return add(num1, num2);
+  } else if (operator == "-") {
+    return subtract(num1, num2);
+  } else if (operator == "*") {
+    return multiply(num1, num2);
+  } else if (operator == "/") {
+    return divide(num1, num2);
+  } else {
+    return "Valid operator please";
+  }
 }
 
-let result = operate(2,2,"/");
+let result;
+
+const numButtons = document.querySelectorAll(".num");
+numButtons.forEach((button) => {
+  button.addEventListener(
+    "click",
+    () => (display.textContent = button.textContent)
+  );
+});
 
 const display = document.getElementById("display");
 display.textContent = result;
